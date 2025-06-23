9d5ee38c719d6364e8b15e1c0c6bf21d5ef633dc
violabirech <violabirech@gmail.com>
2025-06-24 00:43:13 +1000

    Update code.py

--- code.py
+++ code.py
@@ -354,12 +354,12 @@ with tabs[4]:
         st.warning("No historical data available from InfluxDB.")
 
 elif dashboard_choice == "DoS":
-    # DoS-specific InfluxDB config
-    INFLUXDB_URL = "https://us-east-1-1.aws.cloud2.influxdata.com"
-    INFLUXDB_ORG = "Anormally Detection"
-    INFLUXDB_BUCKET = "realtime"
-    INFLUXDB_TOKEN = "DfmvA8hl5EeOcpR-d6c_ep6dRtSRbEcEM_Zqp8-1746dURtVqMDGni4rRNQbHouhqmdC7t9Kj6Y-AyOjbBg-zg=="
-    DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1383262825534984243/mMaPgCDV7tgEMsT_-5ABWpnxMJB746kM_hQqFa2F87lRKeBqCx9vyGY6sEyoY4NnZ7d7"
+# DoS-specific InfluxDB config
+INFLUXDB_URL = "https://us-east-1-1.aws.cloud2.influxdata.com"
+INFLUXDB_ORG = "Anormally Detection"
+INFLUXDB_BUCKET = "realtime"
+INFLUXDB_TOKEN = "DfmvA8hl5EeOcpR-d6c_ep6dRtSRbEcEM_Zqp8-1746dURtVqMDGni4rRNQbHouhqmdC7t9Kj6Y-AyOjbBg-zg=="
+DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1383262825534984243/mMaPgCDV7tgEMsT_-5ABWpnxMJB746kM_hQqFa2F87lRKeBqCx9vyGY6sEyoY4NnZ7d7"
 # --- Helper Functions ---
 def query_influx(start_range="-1h", limit=300):
     try:
