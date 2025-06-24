0b8986b9acd6c987e49244b44c8305c53eb3b305
violabirech <violabirech@gmail.com>
2025-06-24 16:59:43 +1000

    Update new.py

--- new.py
+++ new.py
@@ -1,6 +1,3 @@
-import streamlit as st
-from dns_dashboard import run_dns_dashboard
-from dos_dashboard import run_dos_dashboard
 
 st.set_page_config(page_title="Unified Network Anomaly Dashboard", layout="wide")
 st.title("Real-Time Network Anomaly Detection")
