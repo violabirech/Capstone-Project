721a4beaba5a6a8cfd34787fb3469d1c75aca8ef
violabirech <violabirech@gmail.com>
2025-06-24 15:42:22 +1000

    Update dns_dashboard.py

--- dns_dashboard.py
+++ dns_dashboard.py
@@ -8,6 +8,8 @@ from influxdb_client import InfluxDBClient
 from streamlit_autorefresh import st_autorefresh
 import plotly.express as px   
 
+st.set_page_config(page_title="DNS Anomaly Detection Dashboard", layout="wide")
+
 # InfluxDB config
 INFLUXDB_URL = "https://us-east-1-1.aws.cloud2.influxdata.com"
 INFLUXDB_ORG = "Anormally Detection"
@@ -342,4 +344,4 @@ with tabs[4]:
     else:
         st.warning("No historical data available from InfluxDB.")
 
-st.set_page_config(page_title="DNS Anomaly Detection Dashboard", layout="wide")
+
