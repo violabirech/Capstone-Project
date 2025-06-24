6e0ffbd6da20c5d11d67916bae3f80a634dbeae1
violabirech <violabirech@gmail.com>
2025-06-24 16:33:57 +1000

    Update new.py

--- new.py
+++ new.py
@@ -1,24 +1,13 @@
 import streamlit as st
-import os
+from dns_dashboard import run_dns_dashboard
+from dos_dashboard import run_dos_dashboard
 
 st.set_page_config(page_title="Unified Network Anomaly Dashboard", layout="wide")
 st.title("Real-Time Network Anomaly Detection")
 
-# Toggle
-dashboard_choice = st.radio("Select a Dashboard:", ["DNS", "DoS"], horizontal=True)
+dashboard = st.radio("Select a Dashboard:", ["DNS", "DoS"], horizontal=True)
 
-# Map each dashboard to its file
-dashboard_files = {
-    "DNS": "dns_dashboard.py",
-    "DoS": "dos_dashboard.py"
-}
-
-# Execute the selected file
-selected_file = dashboard_files[dashboard_choice]
-
-try:
-    with open(selected_file, 'r', encoding='utf-8') as f:
-        code = f.read()
-        exec(code, globals())
-except Exception as e:
-    st.error(f"Failed to load {selected_file}: {e}")
+if dashboard == "DNS":
+    run_dns_dashboard()
+else:
+    run_dos_dashboard()
