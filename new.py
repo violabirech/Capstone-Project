f82d95dca32b2a2edb8a430b0148bde7c3d40029
violabirech <violabirech@gmail.com>
2025-06-24 16:54:37 +1000

    Update new.py

--- new.py
+++ new.py
@@ -6,8 +6,7 @@ st.set_page_config(page_title="Unified Network Anomaly Dashboard", layout="wide"
 st.title("Real-Time Network Anomaly Detection")
 
 dashboard = st.radio("Select a Dashboard:", ["DNS", "DoS"], horizontal=True)
-
-if dashboard == "DNS":
-    run_dns_dashboard()
+if dashboard_choice == "DNS":
+    load_dashboard("dns_dashboard")
 else:
-    run_dos_dashboard()
+    load_dashboard("dos_dashboard")
