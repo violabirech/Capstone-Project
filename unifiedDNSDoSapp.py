80276d2524bf9c6a3338c64ef3b2cd61941bd700
violabirech <violabirech@gmail.com>
2025-06-24 10:33:57 +1000

    Update unifiedDNSDoSapp.py

--- unifiedDNSDoSapp.py
+++ unifiedDNSDoSapp.py
@@ -11,6 +11,8 @@ import plotly.express as px
 # Set up page
 st.set_page_config(page_title="Unified Network Anomaly Dashboard", layout="wide")
 st.title("Real-Time Network Anomaly Detection")
+if "predictions" not in st.session_state:
+    st.session_state.predictions = []
 
 # Dashboard toggle
 dashboard_choice = st.radio("Select a Dashboard:", ["DNS", "DoS"], horizontal=True)
