27a68b0ed6dc26095689f27222d66ba31af1bd27
violabirech <violabirech@gmail.com>
2025-06-24 14:51:01 +1000

    Update dos_dashboard.py

--- dos_dashboard.py
+++ dos_dashboard.py
@@ -1,15 +1,3 @@
-# Safe session state defaults
-if "threshold" not in st.session_state:
-    st.session_state["threshold"] = -1.0
-
-if "highlight_color" not in st.session_state:
-    st.session_state["highlight_color"] = "green"
-
-if "selected_range" not in st.session_state:
-    st.session_state["selected_range"] = "Last 1 hour"
-
-if "alerts_enabled" not in st.session_state:
-    st.session_state["alerts_enabled"] = True
 import streamlit as st
 import pandas as pd
 import numpy as np
