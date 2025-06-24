991f325e5ebed0ac37e1ff0bfc6c12ccd6e4876d
violabirech <violabirech@gmail.com>
2025-06-24 17:56:54 +1000

    Update new.py

--- new.py
+++ new.py
@@ -1,13 +1,15 @@
+# app.py
+
 import streamlit as st
 from dns_dashboard import show_dns_dashboard
 from dos_dashboard import show_dos_dashboard
 
 st.set_page_config(page_title="Unified Network Anomaly Detection", layout="wide")
-st.title("Real-Time Network Anomaly Detection")
+st.title("🔍 Real-Time Network Anomaly Detection")
 
 dashboard_choice = st.radio("Select a Dashboard:", ["DNS", "DoS"], horizontal=True)
 
 if dashboard_choice == "DNS":
     show_dns_dashboard()
-elif dashboard_choice == "DoS":
+else:
     show_dos_dashboard()
