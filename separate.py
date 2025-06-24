7802a63f4c6869f346021588e1c31824d3c57c77
violabirech <violabirech@gmail.com>
2025-06-24 13:09:47 +1000

    Update separate.py

--- separate.py
+++ separate.py
@@ -1,16 +1,14 @@
 import streamlit as st
 
-# Set Streamlit page config
+# Set page config
 st.set_page_config(page_title="Unified Network Anomaly Dashboard", layout="wide")
 st.title("Real-Time Network Anomaly Detection")
 
-# Radio button toggle
+# Toggle between dashboards
 dashboard_choice = st.radio("Select a Dashboard:", ["DNS", "DoS"], horizontal=True)
 
-# Load and execute the correct dashboard
+# Load respective dashboard
 if dashboard_choice == "DNS":
-    with open("dns_dashboard.py") as f:
-        exec(f.read())
-elif dashboard_choice == "DoS":
-    with open("dos_dashboard.py") as f:
-        exec(f.read())
+    exec(open("dns_dashboard.py").read())
+else:
+    exec(open("dos_dashboard.py").read())
